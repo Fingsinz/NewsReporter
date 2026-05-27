0d389c4a8f0ec570873b3d415a1c1a9f7a108d6a
Fingsinz <fingsinz@outlook.com>
2026-05-27 20:56:09 +0800

    feat(frame): add homepage

--- frame/__init__.py
+++ frame/__init__.py
@@ -1,6 +1,7 @@
 """Page frame module."""
 
 from .html_generator import MarkdownToHTMLConverter
+from .homepage import generate_homepage
 
-__all__ = ["MarkdownToHTMLConverter"]
+__all__ = ["MarkdownToHTMLConverter", "generate_homepage"]
 __version__ = "0.1.0"
